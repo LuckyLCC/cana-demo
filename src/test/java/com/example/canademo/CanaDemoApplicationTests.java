33247b2b722fee5533153016132afc2572e1cc9d
liuchang <liuchang061416!@#>
2022-10-14 10:50:48 +0800

    Initial commit

---
+++ src/test/java/com/example/canademo/CanaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
