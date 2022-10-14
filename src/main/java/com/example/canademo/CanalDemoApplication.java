2efbd128030b0835bd795c0e1b966ccffa72e045
liuchang <liuchang061416!@#>
2022-10-14 16:05:46 +0800

    222333

--- src/main/java/com/example/canademo/CanalDemoApplication.java
+++ src/main/java/com/example/canademo/CanalDemoApplication.java
@@ -8,12 +8,14 @@ import com.example.canademo.utils.CanalConfig;
 import com.example.canademo.utils.DataInfo;
 import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLOutput;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -27,26 +29,29 @@ public class CanalDemoApplication {
         CanalConfig canalConfig = new CanalConfig();
         service.execute(canalConfig);
 
-        while (true){
+        while (true) {
             Thread.sleep(5000);
 //            List<String> data1 = canalConfig.getData();
             ConcurrentLinkedQueue<String> clq = DataInfo.getClq();
             String poll = clq.poll();
-            System.out.println("取出的数据为"+poll);
+            System.out.println("取出的数据为" + poll);
 
-//            JSONObject jsonObject = JSONObject.parseObject(poll);
-//            String type = (String) jsonObject.get("type");
+            if (StringUtils.isEmpty(poll)) {
+                continue;
+            }
+
+            JSONObject jsonObject = JSONObject.parseObject(poll);
+            if (jsonObject.isEmpty()) {
+                continue;
+            }
+
+            String type = (String) jsonObject.get("sex");
+            System.out.println("sex为" + type);
             IStrategy strategy = StrategyFactory.getStrategy("baseInfoVehicle");
             String s = strategy.handleData(poll);
             strategy.sendData(s);
-            System.out.println(Thread.currentThread().getName()+"-----------------");
+            System.out.println(Thread.currentThread().getName() + "-----------------");
         }
-
-
-
-
-
-
 
 
     }
